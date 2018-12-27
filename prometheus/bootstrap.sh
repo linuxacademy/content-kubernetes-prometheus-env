2540ce0db365fa4b2e63802a87afdd08a70229f3
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2018-12-26 18:23:29 -0600

    add bootstrap file

---
+++ prometheus/bootstrap.sh
@@ -0,0 +1,6 @@
+kubectl apply -f clusterRole.yml
+kubectl apply -f namespaces.yml
+kubectl apply -f prometheus-config-map.yml
+kubectl apply -f prometheus-rules-config-map.yml
+kubectl apply -f prometheus-deployment.yml
+kubectl apply -f kube-state-metrics.yml
