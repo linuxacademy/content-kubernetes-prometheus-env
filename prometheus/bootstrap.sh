407a5bdda45d890b77b4ad3716ac2588c9ebb030
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2018-12-28 10:33:15 -0600

    setup lab

--- prometheus/bootstrap.sh
+++ prometheus/bootstrap.sh
@@ -1,6 +1,5 @@
 kubectl apply -f clusterRole.yml
 kubectl apply -f namespaces.yml
 kubectl apply -f prometheus-config-map.yml
-kubectl apply -f prometheus-rules-config-map.yml
 kubectl apply -f prometheus-deployment.yml
 kubectl apply -f kube-state-metrics.yml
